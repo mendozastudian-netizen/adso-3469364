35ef42433804d5576c91a1316cf31d5b903abc6d
Ivan <mendozastudian@gmail.com>
2026-05-08 17:25:30 -0500

    prueba 2

---
+++ PYTHON/Taller_01/taller.py
@@ -0,0 +1,7 @@
+#Solicitar base y altura de un triángulo y calcular su área: Área = (base * altura) / 2
+triangulo= ("base*altura/2")
+base=int(input("ingrese el num de la base"))
+altura=int(input("ingrese el num de la altura"))
+multiplicacion=base*altura/2 
+print("la multiplicacion es:", multiplicacion)
+#Solicitar el radio de un círculo y calcular su área: Área = π * radio² (usar π = 3.1416)
